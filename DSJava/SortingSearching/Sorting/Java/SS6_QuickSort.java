1e49b2886134ddfe94498f89803100461985cbf2
parimal-selldo <parimal.mahindrakar@sell.do>
2023-08-20 20:56:39 +0530

    [JAVA] Refactor the code

--- DSJava/SortingSearching/Sorting/Java/SS6_QuickSort.java
+++ DSJava/SortingSearching/Sorting/Java/SS6_QuickSort.java
@@ -1,16 +1,13 @@
 package Sorting;
 public class SS6_QuickSort {
 
-
     public static void swap(int[] arr, int i, int j)  {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-
-    static int partition(int[] arr, int low, int high)
-    {
+    static int partition(int[] arr, int low, int high) {
 
         // pivot
         int pivot = arr[high];
@@ -22,11 +19,11 @@ public class SS6_QuickSort {
 
         for (int j = low; j <= high - 1; j++) {
 
-            // If current element is smaller 
+            // If current element is smaller
             // than the pivot
             if (arr[j] < pivot) {
 
-                // Increment index of 
+                // Increment index of
                 // smaller element
                 i++;
                 swap(arr, i, j);
@@ -35,17 +32,14 @@ public class SS6_QuickSort {
         swap(arr, i + 1, high);
         return (i + 1);
     }
-    
 
-    static void quickSort(int[] arr, int low, int high)
-    {
-        if (low < high) 
-        {
-            
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+
             // pi is partitioning index, arr[p]
-            // is now at right place 
+            // is now at right place
             int pi = partition(arr, low, high);
-    
+
             // Separately sort elements before
             // partition and after partition
             quickSort(arr, low, pi - 1);
@@ -53,20 +47,17 @@ public class SS6_QuickSort {
         }
     }
 
-
     public static void printArray(int arr[])  {
         int n = arr.length;
         for (int i=0; i<n; ++i)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
-    
+
     public static void main(String[] args) {
         int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
         printArray(arr);
         quickSort(arr, 0, arr.length - 1);
         printArray(arr);
-        
     }
-    
 }
